20a4ae88280f898a5aa71dc2035ee9c3c708f073
BB9z <bb9z@me.com>
2018-03-28 22:22:25 +0800

    #test RFMessage and RFMessageManager.

---
+++ Test/Shared/Test_RFMessage.swift
@@ -0,0 +1,50 @@
+//
+//  Test_RFMessage.swift
+//  Test-iOS
+//
+//  Created by BB9z on 28/03/2018.
+//  Copyright © 2018 RFUI. All rights reserved.
+//
+
+import XCTest
+
+class Test_RFMessage: XCTestCase {
+    func testCreateWithConfig() {
+        do {
+            let msg = try RFMessage { m in
+                
+            }
+            XCTAssertNil(msg)
+        } catch {
+            XCTAssert((error as NSError).code > 0)
+            XCTAssertNotNil(error)
+        }
+        
+        let msg = try? RFMessage { m in
+            m.identifier = "zz"
+        }
+        XCTAssertNotNil(msg)
+    }
+    
+    func testCreateWithInit() {
+        let msg1 = RFMessage(identifier: "zz")
+        XCTAssertNotNil(msg1)
+        
+        let expectation = XCTestExpectation(description: "wait for assert fail in after init")
+        let msg2 = RFMessage()
+        XCTAssertNotNil(msg2, "It's not be nil, but will throw an NSInternalInconsistencyException later")
+        wait(for: [expectation], timeout: 1)
+    }
+    
+    func testEuqal() {
+        let msg = try? RFMessage { m in
+            m.identifier = "zz"
+        }
+        let msg2 = try? RFMessage { m in
+            m.identifier = (msg?.identifier)!
+        }
+        XCTAssertEqual(msg, msg2)
+        XCTAssert(msg == msg2)
+        XCTAssert(msg?.hash == msg2?.hash)
+    }
+}
--- Test/Shared/Test_RFMessageManager.swift
+++ Test/Shared/Test_RFMessageManager.swift
@@ -14,4 +14,76 @@ class Test_RFMessageManager: XCTestCase {
         return RFMessageManager()
     }()
     
+    func testHide() {
+        let m1 = RFMessage(identifier: "ID_1")
+        let m2 = RFMessage(identifier: "ID_2")
+        let m3 = RFMessage(identifier: "ID_3")
+        
+        manager.show(m1)
+        manager.show(m2)
+        manager.show(m3)
+        XCTAssertEqual(manager.displayingMessage, m1)
+        XCTAssertEqual(manager.queuedMessages, [m2, m3])
+
+        manager.hide(withIdentifier: "ID_1")
+        XCTAssertEqual(manager.displayingMessage, m2)
+        XCTAssertEqual(manager.queuedMessages, [m3])
+        
+        manager.hide(withIdentifier: "ID_3")
+        XCTAssertEqual(manager.displayingMessage, m2)
+        XCTAssertEqual(manager.queuedMessages, [])
+        
+        manager.show(m1)
+        manager.show(m3)
+        XCTAssertEqual(manager.displayingMessage, m2)
+        XCTAssertEqual(manager.queuedMessages, [m1, m3])
+        
+        manager.hide(withIdentifier: nil)
+        XCTAssertEqual(manager.displayingMessage, nil)
+        XCTAssertEqual(manager.queuedMessages, [])
+    }
+    
+    func testHideGroup() {
+        let m1 = RFMessage(identifier: "ID_1")
+        m1.groupIdentifier = "g1"
+        let m2 = RFMessage(identifier: "ID_2")
+        m2.groupIdentifier = "g2"
+        let m3 = RFMessage(identifier: "ID_3")
+        m3.groupIdentifier = "g3"
+        
+        manager.show(m1)
+        manager.show(m2)
+        manager.show(m3)
+        print(manager)
+        manager.hide(withGroupIdentifier: "g3")
+        XCTAssertEqual(manager.displayingMessage, m1)
+        XCTAssertEqual(manager.queuedMessages, [m2])
+        
+        manager.hide(withGroupIdentifier: "g1")
+        XCTAssertEqual(manager.displayingMessage, m2)
+        XCTAssertEqual(manager.queuedMessages, [])
+        
+        manager.show(m1)
+        manager.show(m2)
+        manager.show(m3)
+        manager.hide(withGroupIdentifier: nil)
+        XCTAssertEqual(manager.displayingMessage, nil)
+        XCTAssertEqual(manager.queuedMessages, [])
+    }
+    
+    func testMultipleIdenticalIDs() {
+        let m1 = RFMessage(identifier: "ID_e")
+        let m2 = RFMessage(identifier: "ID_e")
+        let m3 = RFMessage(identifier: "ID_e")
+        
+        manager.show(m1)
+        manager.show(m2)
+        manager.show(m3)
+        XCTAssertEqual(manager.displayingMessage, m1)
+        XCTAssertEqual(manager.queuedMessages, [m2], "m3 is same to m2, so m3 not added into queue.")
+        
+        manager.hide(withIdentifier: "ID_e")
+        XCTAssertEqual(manager.displayingMessage, nil)
+        XCTAssertEqual(manager.queuedMessages, [])
+    }
 }
