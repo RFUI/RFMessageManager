09cbe6a4f4f1b1a2e09e937de127e060f3c859fa
BB9z <bb9z@me.com>
2018-04-24 11:29:51 +0800

    #test fix.

--- Test/macOS/OC-Bridging-Header.h
+++ Test/macOS/OC-Bridging-Header.h
@@ -2,9 +2,7 @@
 //  Use this file to import your target's public headers that you would like to expose to Swift.
 //
 
+#import "RTHelper.h"
 #import <RFMessageManager/RFMessageManager.h>
 #import <RFMessageManager/RFMessageManager+RFDisplay.h>
-#if !TARGET_OS_OSX
 #import <RFMessageManager/RFSVProgressMessageManager.h>
-#import "MBNavigationItem.h"
-#endif
